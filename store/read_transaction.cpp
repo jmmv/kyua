6348bf0f925a316d58885cd8c6398ff4dfe1e9ed
Enji Cooper <yaneurabeya@gmail.com>
2019-05-19 22:59:41 -0700

    Provide a bogus result (a newline) when a skipped result has no context
    This allows kyua to function in a backwards compatible manner, allowing
    previous versions to read databases from newer versions.
    
    Signed-off-by: Enji Cooper <yaneurabeya@gmail.com>

--- store/read_transaction.cpp
+++ store/read_transaction.cpp
@@ -196,22 +196,13 @@ parse_result(sqlite::statement& stmt, const char* type_column,
     try {
         const model::test_result_type type =
             store::column_test_result_type(stmt, type_column);
-        switch (type) {
-        case model::test_result_passed:
+        if (type == model::test_result_passed) {
             if (stmt.column_type(stmt.column_id(reason_column)) !=
                 sqlite::type_null)
                 throw store::integrity_error("Result of type 'passed' has a "
                                              "non-NULL reason");
             return model::test_result(type);
-        /// Some test interfaces don't have to provide a reason when skipping a
-        /// test, e.g., Google Test.
-        case model::test_result_skipped:
-            if (stmt.column_type(stmt.column_id(reason_column)) ==
-                sqlite::type_null) {
-                return model::test_result(type);
-            }
-            /// FALLTHROUGH
-        default:
+        } else {
             return model::test_result(type,
                                       stmt.safe_column_text(reason_column));
         }
